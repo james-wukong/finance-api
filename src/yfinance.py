c027f0eed9195838df28d5c911263bd1785c76a3
James Cheng <qianfan.cheng.cn@gmail.com>
2024-02-23 16:23:03 -0500

    Update yfinance.py

--- src/yfinance.py
+++ src/yfinance.py
@@ -8,33 +8,6 @@ class YFApi(ApiInterface):
     """
     Base class that implements api calls
     """
-
-    def __init__(self,
-                 base_url=None,
-                 api_key=None,
-                 output_format='json',
-                 write_to_mysql=False,
-                 write_to_postgres=False,
-                 write_to_azure=False,
-                 write_to_mongo=False,
-                 mongo_conf: dict = None,
-                 hadoop_conf: dict = None,
-                 maria_conf: dict = None,
-                 azure_conf: dict = None,
-                 postgres_conf: dict = None):
-        super(YFApi, self).__init__(base_url=base_url,
-                                    api_key=api_key,
-                                    output_format=output_format,
-                                    write_to_mysql=write_to_mysql,
-                                    write_to_postgres=write_to_postgres,
-                                    write_to_azure=write_to_azure,
-                                    write_to_mongo=write_to_mongo,
-                                    mongo_conf=mongo_conf,
-                                    hadoop_conf=hadoop_conf,
-                                    maria_conf=maria_conf,
-                                    azure_conf=azure_conf,
-                                    postgres_conf=postgres_conf)
-
     # @ApiDecorator.write_to_hadoop_csv(file_name='yf_historical_data')
     @ApiDecorator.write_to_maria_sp(write_table='yf_historical_data')
     @ApiDecorator.write_to_postgres_sp(write_table='yf_historical_data')
