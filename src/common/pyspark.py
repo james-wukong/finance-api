e9c3984ce542bf09f23e6273bae7aa22441f06db
James Cheng <qianfan.cheng.cn@gmail.com>
2024-02-23 19:40:07 -0500

    update

--- src/common/pyspark.py
+++ src/common/pyspark.py
@@ -9,7 +9,7 @@ class MySpark:
     Initialize the spark
     """
     @classmethod
-    def initialize_spark(cls, mongo_uri: str, name: str = 'Default Project', is_azure: bool = False) -> SparkSession | None:
+    def initialize_spark(cls, mongo_uri: str, name: str = 'Default Project') -> SparkSession | None:
         """
         initialize spark
         :param is_azure:
@@ -19,20 +19,13 @@ class MySpark:
         """
         try:
             # getting the spark instance
-            if is_azure:
-                spark = SparkSession.builder \
-                    .appName(name) \
-                    .config("spark.mongodb.input.uri", mongo_uri) \
-                    .config("spark.mongodb.output.uri", mongo_uri) \
-                    .config("spark.jars.packages", "com.microsoft.azure:spark-mssql-connector_2.12:1.2.0") \
-                    .getOrCreate()
-            else:
-                spark = SparkSession.builder \
-                    .appName(name) \
-                    .config("spark.mongodb.input.uri", mongo_uri) \
-                    .config("spark.mongodb.output.uri", mongo_uri) \
-                    .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:3.0.1") \
-                    .getOrCreate()
+            spark = SparkSession.builder \
+                .appName(name) \
+                .config("spark.mongodb.input.uri", mongo_uri) \
+                .config("spark.mongodb.output.uri", mongo_uri) \
+                .config("spark.jars.packages", "com.microsoft.azure:spark-mssql-connector_2.12:1.2.0,"
+                                               "org.mongodb.spark:mongo-spark-connector_2.12:3.0.1") \
+                .getOrCreate()
         except PySparkException as e:
             print(f"Failed to get or create Spark: {e}")
 
